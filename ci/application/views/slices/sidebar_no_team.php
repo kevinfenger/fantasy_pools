c0767bbaad9842707c035f3fad594a98a73969bd
Kevin Fenger <kevindfenger@gmail.com>
2015-01-03 10:16:29 -0600

    fixing modal load issue, and blank page when session times out on league page

---
+++ ci/application/views/slices/sidebar_no_team.php
@@ -0,0 +1,4 @@
+  <div class="span4">
+    <h3>My Team</h3>
+    <p>You are not a member of this league. If you feel you have reached this page in error, please check your login status.</p> 
+  </div>
