aebd3b7473e3d3b8078b689d7c3b17585a44742d
Kevin Fenger <kevindfenger@gmail.com>
2015-01-03 15:52:47 -0600

    fixing nonowner view for mobile

--- ci/application/views/pages/team_nonowner_view.php
+++ ci/application/views/pages/team_nonowner_view.php
@@ -2,6 +2,7 @@
   <div class="span8">
     <h3><a href="league?league_id=<?php echo $league_id?>"><?php echo $league_name; ?></a></h3>
     <h4><?php echo $team_details->team_name . ' (' . $team_details->first_name . ' ' . $team_details->last_name . ')'; ?></h4>
+    <div id="no-more-tables"> 
     <table id="team_info" class="table table-striped">
       <thead>
         <tr>
@@ -18,22 +19,23 @@
       <tbody>
         <?php foreach ($team_players as $tm):?>
         <tr class="player_position" value=<?php echo $tm['player']['player_id']; ?>> 
-          <td class="player_position"><?php echo $tm['position']['position_short_description']; ?></td> 
-          <td class="player_name"><?php if ($tm['player']): ?>
+          <td data-title="Position" class="player_position"><?php echo $tm['position']['position_short_description']; ?></td> 
+          <td data-title="Name" class="player_name"><?php if ($tm['player']): ?>
               <p><?php echo $tm['player']['full_name']; ?></p>
               <?php else: ?> 
               <strong>Not Yet Selected</strong>              
               <?php endif; ?> 
           </td>
-          <td class="player_team"><?php echo $tm['player']['pro_team']; ?></td> 
-          <td>0</td>  
-          <td>0</td>  
-          <td>0</td>  
-          <td>0</td>  
-          <td>0</td>  
+          <td data-title="Team" class="player_team"><?php echo $tm['player']['pro_team']; ?></td> 
+          <td data-title="W1 Points">0</td>  
+          <td data-title="W2 Points">0</td>  
+          <td data-title="W3 Points">0</td>  
+          <td data-title="W4 Points">0</td>  
+          <td data-title="Total Points">0</td> 
         </tr>
         <?php endforeach; ?>
       </tbody>
     </table>
+    </div> 
   </div>
 </div> 
