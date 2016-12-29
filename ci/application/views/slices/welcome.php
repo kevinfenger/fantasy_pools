8c65a7444ba80dba3e1c83762dc2883710bfcb85
Kevin Fenger <fenger@neon-lab.com>
2016-12-29 11:11:33 -0800

    few more

--- ci/application/views/slices/welcome.php
+++ ci/application/views/slices/welcome.php
@@ -1,5 +1,9 @@
 <div class="container-table">
  <div class="row vertical-center-row">
-    <div class="text-center col-md-4 col-md-offset-4" style="background:red">Welcome</div>
+    <div class="text-center col-md-4 col-md-offset-4">
+      <h2>Welcome</h2>
+      <h4>Get Started by Logging In</h4> 
+      <a class="btn" href="<?php echo base_url('login'); ?>">Login</a>
+    </div>
  </div>
 </div>
