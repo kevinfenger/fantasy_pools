2e8b755708e239a142ea94850c4bb15fb57dfe5c
Kevin Fenger <fenger@neon-lab.com>
2016-12-29 11:29:23 -0800

    button styling

--- ci/application/views/slices/welcome.php
+++ ci/application/views/slices/welcome.php
@@ -1,7 +1,7 @@
 <div class="container-table">
  <div class="row vertical-center-row">
     <div class="text-center col-md-9 col-md-offset-2">
-      <h1>Welcome to Fantasy Pools</h1>
+      <h1>Fantasy Pools</h1>
       <h3>Get Started by Logging In</h3> 
       <br>
       <br>  
