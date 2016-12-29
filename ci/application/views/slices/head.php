2188360e869d6f7f6203d90dae07d85113999194
Kevin Fenger <fenger@neon-lab.com>
2016-12-28 18:37:02 -0800

    update css

--- ci/application/views/slices/head.php
+++ ci/application/views/slices/head.php
@@ -12,14 +12,15 @@
 
 <!-- crayons and paint -->	
 <?php echo //add_css(array('bootstrap', 'bootstrap-responsive', 'style')); 
-          add_css(array('//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css', '//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css'));
+          //add_css(array('//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css', '//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css'));
+          add_css(array('bootstrap', 'bootstrap-min', '1-col-portfolio'));
           //add_css(array('//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/2.3.1/css/bootstrap.min.css', '//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/2.3.1/css/bootstrap-responsive.min.css', 'core'));
 //          add_css(array('//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css', '//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/2.3.1/css/bootstrap-responsive.min.css', 'style'));
 ?>
 <?php echo $css; ?>
 
 <!-- magical wizardry -->
-<?php echo jquery('1.9.1'); ?>
+<?php echo jquery('1.11.1'); ?>
 <?php echo shiv(); ?>
 <?php echo add_js(array('//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js', 'scripts', 'fb_login', 'login_funcs')); ?>
 <?php echo $js; ?>
