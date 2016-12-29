1c0ece6325487f0ce031306780da152b720e7ea9
Kevin Fenger <fenger@neon-lab.com>
2016-12-29 11:21:02 -0800

    finishing up welcome

--- ci/application/views/slices/welcome.php
+++ ci/application/views/slices/welcome.php
@@ -1,8 +1,9 @@
 <div class="container-table">
  <div class="row vertical-center-row">
     <div class="text-center col-md-4 col-md-offset-4">
-      <h2>Welcome</h2>
-      <h4>Get Started by Logging In</h4> 
+      <h1>Welcome to Fantasy Pools</h1>
+      <h3>Get Started by Logging In</h3> 
+      <br> 
       <a class="btn" href="<?php echo base_url('login'); ?>">Login</a>
     </div>
  </div>
