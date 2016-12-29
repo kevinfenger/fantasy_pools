7b2d67f207c2aa9d0113a1cd7507652aae908db2
Kevin Fenger <fenger@neon-lab.com>
2016-12-28 20:14:57 -0800

    basdf

--- ci/application/views/slices/header.php
+++ ci/application/views/slices/header.php
@@ -1,34 +1,32 @@
 <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
   <div class="container">
-    <div class="navbar-header"> 
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="dropdownMenu1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button> 
       <a class="navbar-brand" href="<?php echo base_url(); ?>">Home</a>
-    </div> 
-       <ul class="nav pull-right">
-		  <li class="right">
+    </div>
+    <div class="collapse navbar-collapse" id="dropdownMenu1">  
+       <ul class="nav navbar-nav pull-right">
                <?php 
                    if(!empty($this->session->userdata['id']))
                    {
                       $avatar_url = isset($this->session->userdata['avatar_url']) ? $this->session->userdata['avatar_url'] : null;
                       $first_name = isset($this->session->userdata['first_name']) ? $this->session->userdata['first_name'] : null;
-                      echo "<button class='navbar-toggle' data-toggle='collapse' data-target='dropdownMenu1'>";  
-                      echo "<img class='small_photo' src='$avatar_url'>";
-                      echo "<span class='header_name'>$first_name</span>";
-                      echo "<span class='caret'></span>";
-                      echo "</button>";
-                      echo "<div class='collapse navbar-collapse' id='dropdownMenu1'";
-                      echo "<ul class='nav navbar-nav'>"; 
-                      echo "<li role='presentation'><a role='menuitem' href='#'>Groups</a></li>";
-                      echo "<li role='presentation'><a role='menuitem' href='#'>Games</a></li>";
-                      echo "<li role='presentation'><a role='menuitem' href='#'>Player Lists</a></li>";
-                      echo "<li role='presentation' class='divider'></li>"; 
-                      echo "<li role='presentation' id='logout'><a role='menuitem' href='#'>Logout</a></li>"; 
-                      echo "</ul>";                              
-                      echo "</div>";
+               ?> 
+                      <button class='navbar-toggle' data-toggle='collapse' data-target='dropdownMenu1'>
+                        <img class='small_photo' src="<?php echo $avatar_url?>">
+                        <span class='header_name'><?php echo $first_name; ?></span>
+                      </button> 
+               <?php
                    }
                    else 
                        echo anchor(base_url('login'), 'Login', array('class' => 'navbar-brand')); 
                ?>
-           </li>
        </ul>
+    </div> 
   </div> 
 </div>
