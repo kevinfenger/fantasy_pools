3134b26b82dfcd027878a6f759da4c4522aa3a29
Kevin Fenger <fenger@neon-lab.com>
2016-12-28 18:18:35 -0800

    update css

--- ci/application/views/slices/head.php
+++ ci/application/views/slices/head.php
@@ -12,7 +12,7 @@
 
 <!-- crayons and paint -->	
 <?php echo //add_css(array('bootstrap', 'bootstrap-responsive', 'style')); 
-          add_css(array('//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css'));
+          add_css(array('//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css', '//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css'));
           //add_css(array('//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/2.3.1/css/bootstrap.min.css', '//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/2.3.1/css/bootstrap-responsive.min.css', 'core'));
 //          add_css(array('//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css', '//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/2.3.1/css/bootstrap-responsive.min.css', 'style'));
 ?>
