848a6ccb7333f9861f633017da8361e77a31f1b6
Kevin Fenger <fenger@neon-lab.com>
2016-12-29 11:25:49 -0800

    button styling

--- ci/application/views/slices/welcome.php
+++ ci/application/views/slices/welcome.php
@@ -1,10 +1,11 @@
 <div class="container-table">
  <div class="row vertical-center-row">
-    <div class="text-center col-md-4 col-md-offset-4">
+    <div class="text-center col-md-9 col-md-offset-2">
       <h1>Welcome to Fantasy Pools</h1>
       <h3>Get Started by Logging In</h3> 
-      <br> 
-      <a class="btn" href="<?php echo base_url('login'); ?>">Login</a>
+      <br>
+      <br>  
+      <a class="btn btn-default btn-lg" href="<?php echo base_url('login'); ?>">Login</a>
     </div>
  </div>
 </div>
