5c200428e0cba9e863558ee34b537a4d0cff2f26
Kevin Fenger <fenger@neon-lab.com>
2016-12-29 19:18:47 -0800

    fix a couple typos

--- ci/application/views/pages/register_team_view.php
+++ ci/application/views/pages/register_team_view.php
@@ -8,7 +8,7 @@
 <div class="panel-body">
   <div class="row"> 
     <div class="col-lg-12">
-      <h4>League Information</h4>
+      <h4>Team Information</h4>
     </div>
   </div>  
   <div class="row">
@@ -26,7 +26,7 @@
           <div class="row">
             <div class="col-sm-6"> 
               <span id="unable_to_register_txt" class="verify" style="padding-bottom:10px"></span>
-              <button id="register_team_button" type="submit" class="btn btn-primary">Create League</button>
+              <button id="register_team_button" type="submit" class="btn btn-primary">Register Team</button>
             </div>
           </div>
         </div>
@@ -34,28 +34,3 @@
     </div>
   </div>
 </div>  
-
-<!--fieldset>
-    <legend><?php echo ucfirst($league_name);?> Team Registration</legend>
-  </fieldset>
-  <form class="form-horizontal" action='javascript:void(0);'>
-  <input id="league_id" value=<?php echo $league_id;?> type=hidden> 
-  <div class="control-group">
-    <label class="control-label" for="team_name">Team Name</label>
-    <div class="controls">
-      <input type="text" id="team_name" placeholder="type team name">
-      <span id="team_name_verify_txt" class="verify"></span>
-    </div>
-  </div>    
-  <div class="control-group">
-    <label class="control-label" for="team_image">Team Image</label>
-    <div class="controls">
-      <input type="file" id="team_image">
-    </div>
-  </div>
-  <div class="controls"> 
-    <span id="unable_to_register_txt" class="verify" style="padding-bottom:10px"></span>
-    <button id="register_team_button" type="submit" class="btn btn-primary">Register</button>
-  </div> 
-  </div> 
-  </form-->
