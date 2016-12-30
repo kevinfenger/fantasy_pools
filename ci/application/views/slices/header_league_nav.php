ea07aceb712fddb1c2fef7fd2e3d599094947b6c
Kevin Fenger <fenger@neon-lab.com>
2016-12-30 13:23:08 -0800

    add league settings to a modal

--- ci/application/views/slices/header_league_nav.php
+++ ci/application/views/slices/header_league_nav.php
@@ -16,7 +16,7 @@
          ?>
                  <li><a href="<?php echo base_url('league/edit_settings?league_id='.$league_details['league_id']) ?>">Edit League Settings</a></li>
          <?php endif;?>
-         <li><a href="<?php echo base_url('league/view_settings?league_id='.$league_details['league_id']) ?>">View League Settings</a></li>
+         <li><a href="#" data-toggle="modal" data-target="#league_settings_modal">View League Settings</a></li>
          <li><a href="#" data-toggle="modal" data-target="#league_payout_modal">Payouts</a></li>
          <li><a href="#" data-toggle="modal" data-target="#league_scoring_modal">Scoring</a></li>
        </ul> 
@@ -109,6 +109,37 @@
             ?>
               <tr><td><?php echo $count; ?></td><td>$<?php echo $s; ?></td></tr>
             <?php endforeach; ?>
+            </tbody> 
+          </table>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="modal fade" id="league_settings_modal" tabindex="-1" role="dialog" aria-labelledby="league_settings_modalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title" id="league_payout_modalLabel">League Payouts</h4>
+      </div>
+      <div class="modal-body">
+          <table class="table table-striped" id="modal_table">
+            <p id="loading" style="display:none">Loading</p>
+            <thead>
+              <tr>
+                <td><strong>Setting</strong></td>
+                <td><strong>Value</strong></td>
+              </tr>
+            </thead>
+	    <tbody>
+              <tr><td>League Name</td><td><?php echo $league_details['name']; ?></td></tr>
+              <tr><td>League ID</td><td><?php echo $league_details['league_id']; ?></td></tr>
+              <tr><td>League Password</td><td><?php echo $league_details['league_password']; ?></td></tr>
+              <tr><td>Number of Members</td><td><?php echo $league_details['number_of_members']; ?></td></tr>
+              <tr><td>Maximum Members</td><td><?php echo $league_details['max_members']; ?></td></tr>
+              <tr><td>Commissioner</td><td><?php echo $league_details['commissioner_id']; ?></td></tr>
+              <tr><td>Created</td><td><?php echo $league_details['created']; ?></td></tr>
             </tbody> 
           </table>
       </div>
